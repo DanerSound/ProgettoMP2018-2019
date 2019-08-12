af91f37e2fc2231ac51d276b980a3bab7883f4d7
Andre Cristhian <andre.barreto28@gmail.com>
2019-08-12 14:57:54 +0200

    dummy

--- ProgettoMP2018-2019/src/warehouse/Shelf.java
+++ ProgettoMP2018-2019/src/warehouse/Shelf.java
@@ -8,7 +8,6 @@ public class Shelf {
 
 	private boolean emptyShelf = true;
 	private int availableSpace;
-
 	private ArrayList<Product> shelf;
 
 	public Shelf(Integer spaceInShelf) {
@@ -26,16 +25,16 @@ public class Shelf {
 		}
 	}
 
-	public void setEmptyShelf(boolean emptyShelf) {
-		this.emptyShelf = emptyShelf;
-	}
-
 	public Product pickUpPrduct(int index) {
 		Product currentProduct;
 		currentProduct = shelf.get(index);
 		shelf.remove(index);
 
 		return currentProduct;
+	}
+
+	public void setEmptyShelf(boolean emptyShelf) {
+		this.emptyShelf = emptyShelf;
 	}
 
 	public boolean isFull() {
